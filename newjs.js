6e0d39e479d56230126f64adb0edfc9197eeebd7
Anushka Kamble <anushka.kamble@expleogroup.com>
2023-05-29 17:53:09 +0530

    anushka's commit04

---
+++ newjs.js
@@ -0,0 +1,19 @@
+var v=10;
+const c="Anushka";
+let i=0;
+
+// function 
+
+function print(Arg1){
+    console.log("This is a function");
+}
+
+//Arrow function
+
+const printarrow= (Arg2)=>{
+    console.log("Arrow function");
+}
+
+console.log("function calls:");
+printarrow("arf");
+print(12);
